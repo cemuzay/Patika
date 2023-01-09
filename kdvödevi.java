3709cb6f19972de9d07d128845d1da45a32e7ad7
cem <85490666+cemuzay@users.noreply.github.com>
2023-01-09 04:49:54 +0300

    kdvödevi1

---
+++ kdvödevi.java
@@ -0,0 +1,14 @@
+import javax.sound.midi.SysexMessage;
+import java.util.Scanner;
+
+public class kdvödevi {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("ücret tutarını giriniz : ");
+        double tutar=input.nextDouble();
+       double kdvoranı=0.18;
+       double kdvtutar=tutar* kdvoranı;
+       double kdvlitutar=tutar+kdvtutar;
+        System.out.println(kdvlitutar);
+    }
+}
