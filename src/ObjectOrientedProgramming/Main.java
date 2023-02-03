b7652ba824b988c9c31de1643e71f92c873526d4
cem <85490666+cemuzay@users.noreply.github.com>
2023-02-04 00:06:35 +0300

    Airways

--- src/ObjectOrientedProgramming/Main.java
+++ src/ObjectOrientedProgramming/Main.java
@@ -3,7 +3,9 @@ package src.ObjectOrientedProgramming;
 public class Main {
     public static void main(String[] args) {
         Animal animal=new Animal("asf",124,140);
-       felines felines=new felines("rat",10,20);
-        System.out.println(felines.getName()+" "+felines.getAge()+" "+felines.getWeight());
+       felines felines=new felines("cem",10,20);
+
+        System.out.println(" ismi " +felines.getName()+" yaşı  "+felines.getAge()+" kilosu "+ felines.getWeight() );
+        felines.speak();
     }
 }
